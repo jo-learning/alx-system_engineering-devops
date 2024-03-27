cf3dd62c18dd51846f01574f1c553dcb7d6b595b
yohannes <yohannesguesh01@gmail.com>
2024-03-27 16:43:30 -0300

    CM

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,5 @@
+#!/usr/bin/pup
+# creates a file in /tmp
 file { '/tmp/school':
   content =>'I love Puppet',
   mode => '0744',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# kill process killmenow
 exec { 'pkill':
   command  => 'pkill killmenow',
   provider => 'shell',
